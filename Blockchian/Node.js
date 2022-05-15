ba23eceaba8c847cbb588928bb61fa36f31247f6
mdsajedul <sajedulislms@gmail.com>
2022-05-15 13:06:32 +0600

    public and private key genaration

--- Blockchian/Node.js
+++ Blockchian/Node.js
@@ -1,3 +1,19 @@
+
+const eccrypto = require('eccrypto')
+
+const getPrivateKey = ()=>{
+    const privateKey = eccrypto.generatePrivate();
+    const hexValue = privateKey.toString('base64');
+    return hexValue
+}
+
+const getPublicKey = ()=>{
+    const bufferPrivateKey =new Buffer(getPrivateKey(),'base64')
+    const publicKey = eccrypto.getPublic(bufferPrivateKey) .toString('base64')
+    return publicKey;
+}
+
+
 class Node {
 
     constructor(username,password,email,publicKey,privateKey,role,point){
@@ -15,8 +31,8 @@ class Node {
         this.password = object.password;
         this.email = object.email;
         // need function for create publickey private key and point 
-        this.publicKey = 'newPublicKey'; 
-        this.privateKey = 'newPrivateKey';
+        this.publicKey = getPublicKey();
+        this.privateKey = getPrivateKey();
         this.role = object.role;
         this.point = 5;
     }
